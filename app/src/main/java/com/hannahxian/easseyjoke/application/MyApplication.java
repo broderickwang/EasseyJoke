01d6671de374dd704e9f20c966792bef160fcdaf
marc <wangchengda1990@gmail.com>
2017-04-14 11:31:27 +0800

    添加阿里热修复andfix的依赖

--- app/src/main/java/com/hannahxian/easseyjoke/application/MyApplication.java
+++ app/src/main/java/com/hannahxian/easseyjoke/application/MyApplication.java
@@ -1,6 +1,9 @@
 package com.hannahxian.easseyjoke.application;
 
 import android.app.Application;
+import android.content.pm.PackageManager;
+
+import com.alipay.euler.andfix.patch.PatchManager;
 
 import org.xutils.x;
 
@@ -9,10 +12,22 @@ import org.xutils.x;
  */
 
 public class MyApplication extends Application {
+	public static PatchManager mPatchManager;
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		x.Ext.init(this);
 		x.Ext.setDebug(true);
+
+		mPatchManager = new PatchManager(this);
+		String pkName = this.getPackageName();
+		String versionName = "";
+		try {
+			versionName = this.getPackageManager().getPackageInfo(pkName,0).versionName;
+		} catch (PackageManager.NameNotFoundException e) {
+			e.printStackTrace();
+		}
+		mPatchManager.init(versionName);
+		mPatchManager.loadPatch();
 	}
 }
